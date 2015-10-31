50d2973baf6313af4b2ba1ceaa8a0f75080972d1
炎藤 <weijingjaylin@gmail.com>
2015-10-30 18:30:01 -0700

    first commit

---
+++ data/config/config.ini.php
@@ -0,0 +1,48 @@
+<?php
+defined('InShopNC') or exit('Access Invalid!');
+$config = array();
+$config['shop_site_url'] 		= 'http://192.168.1.4/shop';
+$config['cms_site_url'] 		= 'http://192.168.1.4/cms';
+$config['microshop_site_url'] 	= 'http://192.168.1.4/microshop';
+$config['circle_site_url'] 		= 'http://192.168.1.4/circle';
+$config['admin_site_url'] 		= 'http://192.168.1.4/admin';
+$config['mobile_site_url'] 		= 'http://192.168.1.4/mobile';
+$config['wap_site_url'] 		= 'http://192.168.1.4/wap';
+$config['upload_site_url']		= 'http://192.168.1.4/data/upload';
+$config['resource_site_url']	= 'http://192.168.1.4/data/resource';
+$config['version'] 		= '201401162490';
+$config['setup_date'] 	= '2014-04-12 16:09:02';
+$config['gip'] 			= 0;
+$config['dbdriver'] 	= 'mysqli';
+$config['tablepre']		= 'yhm_';
+$config['db'][1]['dbhost']  	= 'localhost';
+$config['db'][1]['dbport']		= '3306';
+$config['db'][1]['dbuser']  	= 'root';
+$config['db'][1]['dbpwd'] 	 	= 'root';
+$config['db'][1]['dbname']  	= 'yhmall';
+$config['db'][1]['dbcharset']   = 'UTF-8';
+$config['db']['slave'] 		= array();
+$config['session_expire'] 	= 3600;
+$config['lang_type'] 		= 'zh_cn';
+$config['cookie_pre'] 		= '1847_';
+$config['tpl_name'] 		= 'default';
+$config['thumb']['cut_type'] = 'gd';
+$config['thumb']['impath'] = '';
+$config['cache']['type'] 			= 'file';
+//$config['memcache']['prefix']      	= 'nc_';
+//$config['memcache'][1]['port']     	= 11211;
+//$config['memcache'][1]['host']     	= '127.0.0.1';
+//$config['memcache'][1]['pconnect'] 	= 0;
+//$config['redis']['prefix']      	= 'nc_';
+//$config['redis']['master']['port']     	= 6379;
+//$config['redis']['master']['host']     	= '127.0.0.1';
+//$config['redis']['master']['pconnect'] 	= 0;
+//$config['redis']['slave']      	    = array();
+//$config['fullindexer']['open']      = false;
+//$config['fullindexer']['appname']   = 'shopnc';
+$config['debug'] 			= false;
+$config['default_store_id'] = '1';
+// 是否开启伪静态
+$config['url_model'] = false;
+// 二级域名后缀
+$config['subdomain_suffix'] = '';
